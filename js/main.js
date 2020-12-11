f694597903b0e2ba7d954ff79346901484484767
Pavel <pl.hancharou@gmail.com>
2020-12-11 17:16:48 +0300

    feat: create hamburgerMenu()

--- js/main.js
+++ js/main.js
@@ -1,29 +1,54 @@
 'use strict';
 
-const menuToggle = document.querySelector('.menu-toggle');
-const landingsList = document.querySelector('.menu-list__landings');
-const pagesList = document.querySelector('.menu-list__pages');
-const dropListSignIn = document.querySelector('.drop-list__signIn')
-const dropListServices = document.querySelector('.drop-list__services');
+window.addEventListener('DOMContentLoaded', () => {
 
-const openMenu = () => {
-  menuToggle.classList.toggle('menu-toggle__open');
-};
-const openLandingsList = () => {
-  landingsList.classList.toggle('menu-list__landings_active');
-};
-const openPagesList = () => {
-  pagesList.classList.toggle('menu-list__pages_active');
-};
-const openDropListSignIn = () => {
-  dropListSignIn.classList.toggle('drop-list__signIn_active');
-};
-const openDropListServices = () => {
-  dropListServices.classList.toggle('drop-list__services_active');
-};
+  const hamburgerMenu = () => {
+    const menuToggle = document.querySelector('.menu-toggle'),
+      menuListItem = document.querySelectorAll('.menu-list__item'),
+      dropListItem = document.querySelectorAll('.drop-list__item');
 
-menuToggle.addEventListener('click', openMenu);
-landingsList.addEventListener('click', openLandingsList);
-pagesList.addEventListener('click', openPagesList);
-dropListSignIn.addEventListener('click', openDropListSignIn);
-dropListServices.addEventListener('click', openDropListServices);
+    document.addEventListener('click', (e) => {
+      if (e.target.closest('.menu-toggle .hamburger')) {
+        menuToggle.classList.add('menu-toggle__open');
+      } else if (e.target.closest('.menu-toggle .cross') || !e.target.closest('.menu')) {
+        menuToggle.classList.remove('menu-toggle__open');
+      }
+    });
+
+    document.addEventListener('keydown', (e) => {
+      if (e.code === 'Escape' && menuToggle.classList.contains('menu-toggle__open')) {
+        menuToggle.classList.remove('menu-toggle__open');
+      }
+    });
+
+    function toggleList(list, activelist, droplist) {
+      list.forEach(item => item.addEventListener('click', (e) => {
+        e.preventDefault();
+        const listActive = item.lastElementChild.classList.contains(activelist);
+        if ((e.target.matches(droplist) && listActive)) {
+          console.log(e.target);
+          closeList(item, activelist);
+        } else if (e.target.closest(droplist)) {
+          openList(item, activelist);
+        }
+      }));
+    }
+
+    toggleList(menuListItem, 'drop-list__active', '.menu-list__dropdown');
+    toggleList(dropListItem, 'service-list__active', '.drop-list__dropdown');
+
+    function openList(item, selector) {
+      item.style.cssText = 'color: #1565D8; font-weight: 600;';
+      item.children[1].style.fill = '#1565D8';
+      item.lastElementChild.classList.add(selector);
+    }
+
+    function closeList(item, selector) {
+      item.style.cssText = '';
+      item.children[1].style.fill = '';
+      item.lastElementChild.classList.remove(selector);
+    }
+  };
+
+  hamburgerMenu();
+});
